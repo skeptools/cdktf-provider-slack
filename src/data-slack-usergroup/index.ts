e7c81c06a726289e7b0ade2cce48dc95a2a85711
Team Terraform CDK <github-team-tf-cdk@hashicorp.com>
2022-11-22 21:51:33 +0000

    chore: upgrade provider from `0.8.1` to version `0.9.0`

--- src/data-slack-usergroup/index.ts
+++ src/data-slack-usergroup/index.ts
@@ -50,8 +50,8 @@ export class DataSlackUsergroup extends cdktf.TerraformDataSource {
       terraformResourceType: 'slack_usergroup',
       terraformGeneratorMetadata: {
         providerName: 'slack',
-        providerVersion: '0.8.1',
-        providerVersionConstraint: '~> 0.8.1'
+        providerVersion: '0.9.0',
+        providerVersionConstraint: '~> 0'
       },
       provider: config.provider,
       dependsOn: config.dependsOn,
